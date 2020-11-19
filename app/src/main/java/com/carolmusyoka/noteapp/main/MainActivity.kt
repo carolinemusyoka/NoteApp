1f4c1e6070373383c335aa4f51d29b8a9c8c84df
carolinemusyoka <carolmusyoka127@gmail.com>
2020-11-19 11:12:51 +0300

    refactor

--- app/src/main/java/com/carolmusyoka/noteapp/main/MainActivity.kt
+++ app/src/main/java/com/carolmusyoka/noteapp/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.carolmusyoka.noteapp
+package com.carolmusyoka.noteapp.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.carolmusyoka.noteapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
