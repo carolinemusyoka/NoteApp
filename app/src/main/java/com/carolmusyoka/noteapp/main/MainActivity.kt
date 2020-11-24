903357652dac67b2028c5c06a2efe9cef08f998c
carolinemusyoka <carolmusyoka127@gmail.com>
2020-11-24 12:51:45 +0300

    onboarding screens(-datastore)wip

--- app/src/main/java/com/carolmusyoka/noteapp/main/MainActivity.kt
+++ app/src/main/java/com/carolmusyoka/noteapp/main/MainActivity.kt
@@ -25,6 +25,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
 
--- app/src/main/java/com/carolmusyoka/noteapp/onboarding/datastore/OnBoardingPrefManager.kt
+++ app/src/main/java/com/carolmusyoka/noteapp/onboarding/datastore/OnBoardingPrefManager.kt
@@ -4,35 +4,33 @@ package com.carolmusyoka.noteapp.onboarding.datastore
 
 import android.content.Context
 import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.MutablePreferences
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.preferencesKey
 import androidx.datastore.preferences.createDataStore
-import com.carolmusyoka.noteapp.datastore.UIModePreference
-import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
 class OnBoardingPrefManager(context: Context) {
-    private val applicationContext = context.applicationContext
     private val dataStore: DataStore<Preferences> = context.createDataStore(
     name = "onboarding_mode_preference"
     )
 
-    suspend fun setFirstTimeLaunch(isFirstTime: Boolean) {
-        dataStore.edit { preferences ->
-            preferences[IS_FIRST_TIME_LAUNCH] = isFirstTime.toString()
+     suspend fun setFirstTimeLaunch(isFirstTime: Boolean) {
+        dataStore.edit {preferences ->
+            preferences[IS_FIRST_TIME_LAUNCH] = isFirstTime
         }
+
     }
-    val firstTimeLaunch: Flow<Boolean>
-    get() = dataStore.data
+
+    val firstTimeLaunch =  dataStore.data
             .map { preferences ->
                 preferences[IS_FIRST_TIME_LAUNCH] ?: false
 
-            } as Flow<Boolean>
+            }
+
 
 
     companion object {
-         val IS_FIRST_TIME_LAUNCH = preferencesKey<String>("is_first_time")
+        private val IS_FIRST_TIME_LAUNCH = preferencesKey<Boolean>("is_first_time")
     }
 }
